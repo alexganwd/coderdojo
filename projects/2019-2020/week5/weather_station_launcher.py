5660e467bde41b1b0d0c5a6f980f7205ca007577
alex.gandara <alex@alexgan.net>
2020-02-01 12:00:09 +0000

    * Moving if below

--- projects/2019-2020/week5/weather_station_launcher.py
+++ projects/2019-2020/week5/weather_station_launcher.py
@@ -7,12 +7,11 @@ parser.add_argument('--display', action="store_true", default=False)
 
 
 args = parser.parse_args()
-print(args.dry_run)
-print(type(args.dry_run))
 
 if args.dry_run:
     weather_station.run(dry_run=True)
 else:
-    weather_station.run(dry_run=False)
     if args.dispay:
         weather_station.run(dry_run=False, display=True)
+    else:
+        weather_station.run(dry_run=False)
